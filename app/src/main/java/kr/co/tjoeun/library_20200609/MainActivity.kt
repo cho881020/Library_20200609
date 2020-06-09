428d5749b452d54a21a6e501cf79abb03151e3ef
cho881020 <kj_cho@idealidea.co.kr>
2020-06-09 21:20:09 +0900

    앱 설정으로 평문통신 허용 => http주소 이미지도 불러옴

--- app/src/main/java/kr/co/tjoeun/library_20200609/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_20200609/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
-        Glide.with(mContext).load("https://img4.yna.co.kr/photo/cms/2019/05/02/02/PCM20190502000402370_P2.jpg").into(profileImg)
+        Glide.with(mContext).load("http://img4.yna.co.kr/photo/cms/2019/05/02/02/PCM20190502000402370_P2.jpg").into(profileImg)
 
     }
 
