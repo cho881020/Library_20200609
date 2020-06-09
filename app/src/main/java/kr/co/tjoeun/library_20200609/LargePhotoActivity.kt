c6cb17c6dd4436133d07e65b713f0d7183526a6b
cho881020 <kj_cho@idealidea.co.kr>
2020-06-09 20:12:58 +0900

    사진 크게 보기 화면 생성  => 핀치 줌 / 더블터치 시 확대/축소 대응 필요

--- app/src/main/java/kr/co/tjoeun/library_20200609/LargePhotoActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_20200609/LargePhotoActivity.kt
@@ -1,27 +1,18 @@
 package kr.co.tjoeun.library_20200609
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseActivity() {
+class LargePhotoActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_large_photo)
         setupEvents()
         setValues()
     }
 
     override fun setupEvents() {
-
-        profileImg.setOnClickListener {
-
-            val myIntent = Intent(mContext, LargePhotoActivity::class.java)
-            startActivity(myIntent)
-
-        }
 
     }
 
