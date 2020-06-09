91a8f622e41ba82f0f616047dbf0e08c835c3ea5
cho881020 <kj_cho@idealidea.co.kr>
2020-06-09 21:11:02 +0900

    Glide 라이브러리로 웹상의 이미지 불러오기  => INTERNET 권한 사용 명시  => http:// 주소의 이미지는 못불러온다

--- app/src/main/java/kr/co/tjoeun/library_20200609/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_20200609/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.tjoeun.library_20200609
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -26,6 +27,8 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        Glide.with(mContext).load("https://img4.yna.co.kr/photo/cms/2019/05/02/02/PCM20190502000402370_P2.jpg").into(profileImg)
 
     }
 
